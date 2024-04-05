dc03cc3ad3db1a22425f40ef1cbd0a00fe4e1e25
don-yan <yankisen@gmail.com>
2024-04-04 20:04:16 -0400

    bad code

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -2,16 +2,17 @@
 
 
 definePageMeta({
-   layout: 'no-nav'
+  layout: 'no-nav'
 })
 
 </script>
 <template>
 
 
+  <div class="flex flex-col  space-y-4">
   <section class="bg-white dark:bg-gray-900">
     <div
-        class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16 rounded-3xl shadow-2xl shadow-red-600">
+        class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16 rounded-3xl shadow-lg shadow-red-600">
       <div class="flex flex-col justify-center">
         <h1 class="font-don mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Standup Comedy
@@ -23,14 +24,15 @@ definePageMeta({
           <br/>
           With a growing collection of extraordinary venues, rotating lineups of hilarious comics & surprise musical
           guests, get ready to discover an unparalleled underground comedy experience.
+<!--          <br/>
           <br/>
-          <br/>
-          Forget comedy shows, these are comedy events.</p>
+          Forget comedy shows, these are comedy events.-->
+          </p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
-          <a 
-          href="https://www.tickettailor.com/events/acquiredtastecomedy?ref=website"
-             target="_blank"
-             class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-red-900 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+          <a
+              href="https://www.tickettailor.com/events/acquiredtastecomedy?ref=website"
+              target="_blank"
+              class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-red-900 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             Buy Tickets
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                  fill="none" viewBox="0 0 14 10">
@@ -42,12 +44,27 @@ definePageMeta({
         </div>
       </div>
       <div>
-        <iframe class="aspect-video mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
+       <img class="mx-auto w-full lg:max-w-xl h-64 rounded-xl sm:h-96 shadow-xl object-cover" src="/images/promo/standup-comedy-event-arrow.jpg" />
+<!--        <iframe class="aspect-video mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
                 src="https://www.youtube.com/embed/1bV0ljC9CoY" title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
+                --> 
       </div>
     </div>
   </section>
+
+  <section class="dark:bg-gray-900 mt-3">
+    <div
+        class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 gap-8 lg:gap-16 rounded-3xl shadow-lg shadow-red-600">
+
+      <iframe class="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
+              src="https://www.youtube.com/embed/1bV0ljC9CoY" title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+
+    </div>
+  </section>
+    </div>
 
 </template>

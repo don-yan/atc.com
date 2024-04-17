f7d781d844e17cf96935c3f287972a227634c0a4
don-yan <yankisen@gmail.com>
2024-04-17 15:42:10 -0400

    test vercel Function

---
+++ api/ticketTailor.ts
@@ -0,0 +1,53 @@
+/**
+ * Fetch Data from the Ticket Tailor API
+ *
+ * Reference: https://developers.tickettailor.com/
+ */
+
+import {type TTApiResponse} from "../src/@types";
+
+export const config = {
+    runtime: 'edge',
+};
+
+
+export function GET(request: Request) {
+
+    let timestamp = Math.ceil(new Date().getTime() / 1000);
+    let TT_KEY = process.env.TICKET_TAILOR_KEY;
+
+    // NOTE: TT only needs username: https://developers.tickettailor.com/#authentication
+
+    const encodedBasicAuthHeader = Buffer.from(`${TT_KEY}:`).toString('base64')
+
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Authorization", `Basic ${encodedBasicAuthHeader}`);
+
+
+    const requestOptions = {
+        method: 'get',
+        headers: myHeaders,
+        redirect: "follow",
+    };
+
+    let greaterThan = Math.ceil(new Date().getTime() / 1000);
+
+    // TODO: Remove (we are fetching extra events for development)
+    greaterThan = 1704085200 //
+
+    console.log('timestamp', timestamp)
+    return fetch(`https://api.tickettailor.com/v1/events?end_at.gte=${greaterThan}`, requestOptions)
+        .then(async (response) => {
+
+            const json = await response.json();
+            // TODO: sort
+            console.debug(json);
+            json.timestamp = timestamp;
+            return json as TTApiResponse
+        })
+
+    // return new Response(`Hello from ${process.env.VERCEL_REGION} | [${process.env.MAILCHIMP_SERVER_PREFIX}] [${timestamp}]`);
+}
+
+

191c002329f3eec5c38a29f9ad136aad3941e93d
don-yan <yankisen@gmail.com>
2024-04-17 16:15:41 -0400

    refactor shared API methods

--- api/ticketTailor.ts
+++ api/ticketTailor.ts
@@ -5,6 +5,7 @@
  */
 
 import {type TTApiResponse} from "../src/@types";
+import {getEvents} from "../shared/api/ticketTailor";
 
 export const config = {
     runtime: 'edge',
@@ -13,39 +14,14 @@ export const config = {
 
 export async function GET(request: Request) {
 
-    let timestamp = Math.ceil(new Date().getTime() / 1000);
-    let TT_KEY = process.env.TICKET_TAILOR_KEY;
 
-    // NOTE: TT only needs username: https://developers.tickettailor.com/#authentication
+    let TT_KEY = process.env.TICKET_TAILOR_KEY || '';
 
-    const encodedBasicAuthHeader = Buffer.from(`${TT_KEY}:`).toString('base64')
+    let responseData = await getEvents(TT_KEY)
+    console.log('new response')
 
-    const myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json");
-    myHeaders.append("Authorization", `Basic ${encodedBasicAuthHeader}`);
+    return new Response(JSON.stringify(responseData));
 
-
-    const requestOptions = {
-        method: 'get',
-        headers: myHeaders,
-        redirect: "follow",
-    };
-
-    let greaterThan = Math.ceil(new Date().getTime() / 1000);
-
-    // TODO: Remove (we are fetching extra events for development)
-    greaterThan = 1704085200 //
-
-    console.log('timestamp', timestamp)
-    return fetch(`https://api.tickettailor.com/v1/events?end_at.gte=${greaterThan}`, requestOptions)
-        .then(async (response) => {
-
-            const json = await response.json();
-            // TODO: sort
-            console.debug(json);
-            json.timestamp = timestamp;
-            return new Response(JSON.stringify(json));
-        })
 
     // return new Response(`Hello from ${process.env.VERCEL_REGION} | [${process.env.MAILCHIMP_SERVER_PREFIX}] [${timestamp}]`);
 }
---
+++ shared/api/ticketTailor.ts
@@ -0,0 +1,37 @@
+import {type TTApiResponse} from "../../src/@types";
+
+export async function getEvents(apiKey: string): Promise<TTApiResponse> {
+    let timestamp = Math.ceil(new Date().getTime() / 1000);
+
+    // NOTE: TT only needs username: https://developers.tickettailor.com/#authentication
+
+    const encodedBasicAuthHeader = Buffer.from(`${apiKey}:`).toString('base64')
+
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Authorization", `Basic ${encodedBasicAuthHeader}`);
+
+
+    const requestOptions = {
+        method: 'get',
+        headers: myHeaders,
+        redirect: "follow",
+    };
+
+    let greaterThan = Math.ceil(new Date().getTime() / 1000);
+
+    // TODO: Remove (we are fetching extra events for development)
+    greaterThan = 1704085200 //
+
+    console.log('timestamp', timestamp)
+    return fetch(`https://api.tickettailor.com/v1/events?end_at.gte=${greaterThan}`, requestOptions)
+        .then(async (response) => {
+
+            const json = await response.json();
+            // TODO: sort
+            console.debug(json);
+            json.timestamp = timestamp;
+            return json;
+        })
+
+}

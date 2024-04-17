c075550fc176f1bbe1f270dd424220a68762a51e
don-yan <yankisen@gmail.com>
2024-04-17 13:44:01 -0400

    cleanup

--- src/components/forms/ContactForm.vue
+++ src/components/forms/ContactForm.vue
@@ -91,7 +91,8 @@ const submitForm = async () => {
     <div class="container px-5 pt-36 mx-auto">
       <div class="flex flex-col">
         <div class="h-1 bg-gray-200 rounded overflow-hidden">
-          <div class="w-24 h-full bg-red-500"></div>
+          <!--            <div class="w-24 h-full bg-red-500"></div>-->
+          <div class="w-full h-full bg-red-500"></div>
         </div>
         <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
           <h2 class="sm:w-2/5 text-gray-900 font-extrabold title-font text-4xl mb-2 sm:mb-0 dark:text-white">

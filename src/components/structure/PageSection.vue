58a89d17240da12a5a3ae1369e11e7ef7235c05e
don-yan <yankisen@gmail.com>
2024-09-26 21:33:30 -0400

    update padding

--- src/components/structure/PageSection.vue
+++ src/components/structure/PageSection.vue
@@ -17,7 +17,7 @@ const props = defineProps({
           <!--            <div class="w-24 h-full bg-red-500"></div>-->
           <div class="w-full h-full bg-red-500"></div>
         </div>
-        <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+        <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-2">
           <h2 class="sm:w-2/5 text-gray-900 font-extrabold title-font text-4xl mb-2 sm:mb-0 dark:text-white">
             {{ props.title }}
           </h2>
@@ -25,13 +25,13 @@ const props = defineProps({
             <slot name="description">
               {{ props.description }}
             </slot>
-            
+
           </p>
         </div>
       </div>
 
       <div
-          class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 gap-8 lg:gap-16 rounded-3xl- shadow-lg shadow-none shadow-red-600">
+          class="py-8 px-4 mx-auto max-w-screen-xl lg:py-8 gap-8 lg:gap-16 rounded-3xl- shadow-lg shadow-none shadow-red-600">
 
         <slot name="content">
 

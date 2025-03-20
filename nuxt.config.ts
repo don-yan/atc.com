1172ef25b7bbd65801d2f34fb1313bda6b96bada
don-yan <yankisen@gmail.com>
2025-03-20 18:36:10 -0400

    downgrade contentful depdenency to 10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,80 +2,87 @@ import {defineNuxtConfig} from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    alias: {
-        // "~": "./src", // Ensure ~ points to src/
-        "@shared":"./shared"
-    },
-    app: {
-        pageTransition: {name: 'page', mode: 'out-in'},
-    },
-    devtools: {enabled: true},
-    modules: [
-        "@nuxt/image",
+  alias: {
+      // "~": "./src", // Ensure ~ points to src/
+      "@shared":"./shared"
+  },
 
-        // TODO: configure eslint & prettier
-        // REF: https://dev.to/nikitadmitr/configure-eslint-prettier-for-nuxt-3-45f7
-        // "@nuxtjs/eslint-module"
-        "@nuxtjs/tailwindcss",
-        '@pinia/nuxt',
-        "@nuxt/eslint"
-    ],
-    // primevue: {
-    //     // cssLayerOrder: "tailwind-base, primevue, tailwind-utilities, primeflex",
-    //     cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
-    //     components: {
-    //         exclude: ["Editor", "Chart"]
-    //     },
-    //     ripple: true
-    // },
+  app: {
+      pageTransition: {name: 'page', mode: 'out-in'},
+  },
 
-    css: [
-        // TODO: Replace primeicons with another library
-        "primeicons/primeicons.css",
-        '../node_modules/flowbite-vue/dist/index.css'
-        // "assets/scss/primevue-sass-theme-3.50.0/themes/atc-theme/theme.scss",
-        // "primevue/resources/themes/lara-light-purple/theme.css"          ,
+  devtools: {enabled: true},
 
-    ],
-    image: {
-        provider: process.env.VERCEL_ENV ? 'vercel' : 'ipx',
-        ipx: {
+  modules: [
+      "@nuxt/image",
 
-            // Specify the directory where your images are located
-            dir: 'public',
-            // Optionally define allowed domains if you're fetching remote images (not usually needed for local images)
-            // domains: ['localhost'],
-        },
-    },
+      // TODO: configure eslint & prettier
+      // REF: https://dev.to/nikitadmitr/configure-eslint-prettier-for-nuxt-3-45f7
+      // "@nuxtjs/eslint-module"
+      "@nuxtjs/tailwindcss",
+      '@pinia/nuxt',
+      "@nuxt/eslint"
+  ],
 
+  // primevue: {
+  //     // cssLayerOrder: "tailwind-base, primevue, tailwind-utilities, primeflex",
+  //     cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  //     components: {
+  //         exclude: ["Editor", "Chart"]
+  //     },
+  //     ripple: true
+  // },
 
-    runtimeConfig: {
-        private: {
-            CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-            CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-            TICKET_TAILOR_KEY: process.env.TICKET_TAILOR_KEY,
-            MAILCHIMP_KEY: process.env.MAILCHIMP_KEY,
-            MAILCHIMP_SERVER_PREFIX: process.env.MAILCHIMP_SERVER_PREFIX,
-            MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
-        },
-        public: {
-            GTAG_ID: process.env.GTAG_ID
-        }
-    },
-    srcDir: 'src/',
-    ssr: true,
-    typescript: {
-        shim: false, // Disable shims to enforce strict TS
-    },
-    // vue: {
-    //     compilerOptions: {
-    //         // TODO
-    //         // isCustomElement: (tag) =>
-    //         //     ['swiper-slide', 'swiper-container'].includes(tag),
-    //     },
-    // },
-    // alias:{
-    //     '~':'./src'
-    // }
+  css: [
+      // TODO: Replace primeicons with another library
+      "primeicons/primeicons.css",
+      '../node_modules/flowbite-vue/dist/index.css'
+      // "assets/scss/primevue-sass-theme-3.50.0/themes/atc-theme/theme.scss",
+      // "primevue/resources/themes/lara-light-purple/theme.css"          ,
 
+  ],
+
+  image: {
+      provider: process.env.VERCEL_ENV ? 'vercel' : 'ipx',
+      ipx: {
+
+          // Specify the directory where your images are located
+          dir: 'public',
+          // Optionally define allowed domains if you're fetching remote images (not usually needed for local images)
+          // domains: ['localhost'],
+      },
+  },
+
+  runtimeConfig: {
+      private: {
+          CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+          CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+          TICKET_TAILOR_KEY: process.env.TICKET_TAILOR_KEY,
+          MAILCHIMP_KEY: process.env.MAILCHIMP_KEY,
+          MAILCHIMP_SERVER_PREFIX: process.env.MAILCHIMP_SERVER_PREFIX,
+          MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
+      },
+      public: {
+          GTAG_ID: process.env.GTAG_ID
+      }
+  },
+
+  srcDir: 'src/',
+  ssr: true,
+
+  // vue: {
+  //     compilerOptions: {
+  //         // TODO
+  //         // isCustomElement: (tag) =>
+  //         //     ['swiper-slide', 'swiper-container'].includes(tag),
+  //     },
+  // },
+  // alias:{
+  //     '~':'./src'
+  // }
+  typescript: {
+      shim: true, // Disable shims to enforce strict TS
+  },
+
+  compatibilityDate: "2025-03-20"
 })
